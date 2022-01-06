2341fd8c61cb110b60a5e106ea9f42400ad74e01
FabCom <fabcom@communs.fr>
2022-01-06 21:51:32 +0100

    power in progress

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ class Game {
     console.log(this.players)
     if (this.turn_count != 0){
       let turn = new Turn(this, this.players);
-      turn.target_select();
+      turn.character_turn();
       console.log(turn.players)
       this.turn_count -= 1;
     } else {
@@ -50,16 +50,36 @@ class Turn
     this.active_players = this.players.slice();
     this.active_player = null;    
   }
-  
-  target_select()
+
+  character_turn()
   {
     resetDiv("result");
     addElementToDiv("result","Joueurs encore en vie")
-    this.players.filter(player => player.status == "playing").forEach(player => addElementToDiv("result",player.name +" hp: " + player.hp + " mana: " + player.mana + " Class: " + player.class_name))
+    this.players.filter(player => player.status == "playing").forEach(player => addElementToDiv("result",player.name +" hp: " + player.hp + " mana: " + player.mana + " shield: " + player.shield + " Class: " + player.class_name))
     var random_player = Math.floor(Math.random()*this.active_players.length);
     this.active_player = this.active_players[random_player];
     addElementToDiv("target", "C'est au tour de " + this.active_player.name)
     addElementToDiv("target", "Qui attaques-tu ?")
+    this.target_select()
+    this.select_power()
+  }
+  select_power(){
+    let power;
+    let text_btn;
+    let victims = this.players.filter(player => player != this.active_player)
+    if (this.active_player.class_name == "Fighter" && this.active_player.hasEnoughMana(20)){
+      addElementToDiv("target", "Pouvoir spécial")
+      victims.forEach(victim => {this.createButtonDarkVision(this.active_player, victim)})
+    } else if (this.active_player.class_name == "Paladin" && this.active_player.hasEnoughMana(40)){
+      addElementToDiv("target", "Pouvoir spécial")
+      victims.forEach(victim => {this.createButtonHealingLighting(this.active_player, victim)})
+    } else if (this.active_player.class_name == "Assassin" && this.active_player.hasEnoughMana(20)){
+      addElementToDiv("target", "Pouvoir spécial")
+      victims.forEach(victim => {this.createButtonShadowHit(this.active_player, victim)})
+    }
+  }
+  target_select()
+  {
     let victims = this.players.filter(player => player != this.active_player)
     victims.forEach(victim => {this.createButtonTarget(this.active_player, victim)})
   }
@@ -72,16 +92,10 @@ class Turn
     this.active_players = this.active_players.filter(player => player != this.active_player);
     resetDiv("active_players")
     this.active_players.forEach(player => addElementToDiv("active_players", player.name));
-    console.log("players : ")
-    console.log(this.players)
-    console.log("active_players : ")
-    console.log(this.active_players)
-    console.log("Changement de personnage");
     if (this.active_players.length !== 0){
-      this.target_select();
+      this.character_turn();
     } else {
       this.game.begin_turn();
-      console.log("Nouveau tour")
     }
     
   }
@@ -94,6 +108,44 @@ class Turn
     btn.onclick = function(){
       player.dealDamage(victim);
       turn.switch_player();
+    }
+    ResultDiv.appendChild(btn);
+  }
+
+  createButtonDarkVision(player, victim){
+    let turn = this
+    var ResultDiv = document.getElementById('target');
+    var btn = document.createElement("BUTTON");
+    btn.innerHTML = "Dark Vision" + "<br>" + victim.name + "<br>" + "hp: " + victim.hp;
+    btn.onclick = function(){
+      player.darkVision(victim);
+      turn.switch_player();
+      console.log("dark vision")
+    }
+    ResultDiv.appendChild(btn);
+  }
+  createButtonHealingLighting(player, victim){
+    let turn = this
+    var ResultDiv = document.getElementById('target');
+    var btn = document.createElement("BUTTON");
+    btn.innerHTML = "Healing Lighting" + "<br>" + victim.name + "<br>" + "hp: " + victim.hp;
+    btn.onclick = function(){
+      player.healingLighting(victim);
+      turn.switch_player();
+      console.log("Healing Lighting")
+    }
+    ResultDiv.appendChild(btn);
+  }
+  createButtonShadowHit(player, victim){
+    let turn = this
+    var ResultDiv = document.getElementById('target');
+    var btn = document.createElement("BUTTON");
+    btn.innerHTML = "Shadow Hit" + "<br>" + victim.name + "<br>" + "hp: " + victim.hp;
+    btn.onclick = function(){
+      player.shadowHit(victim);
+      turn.switch_player();
+      console.log("shadow")
+
     }
     ResultDiv.appendChild(btn);
   }
--- js/perso.js
+++ js/perso.js
@@ -8,10 +8,21 @@ class Character {
     this.mana = mana;
     this.name = name;
     this.status = status;
+    this.shield = 0
   }
 
-  takeDamage(dmg) { 
-    this.hp -= dmg
+  resetShield(){
+    this.shield = 0
+  }
+  takeDamage(dmg) {
+    let damage = dmg
+    if (this.shield > 0){
+      damage = dmg - this.shield
+      if (damage < 0){
+        damage = 0
+      }
+    }  
+    this.hp -= damage
     if (this.hp <= 0) {
       this.status = "loser"
     }
@@ -25,8 +36,8 @@ class Character {
       this.mana += 20
     }
   }
-  hasEnoughMana(mana_need){
-    if (mana_need < this.mana) {
+  hasEnoughMana(cost){
+    if (cost < this.mana) {
       return true
     } else {
       return false
@@ -35,53 +46,80 @@ class Character {
 }
 
 class Fighter extends Character {
-  constructor(hp=12, dmg=4, mana=40, name="Grace", status){
-    super(hp,dmg,mana,name,status);
+  constructor(hp=12, dmg=4, mana=40, name="Grace",status,shield){
+    super(hp,dmg,mana,name,status,shield);
     this.class_name = "Fighter";
   }
 
   darkVision(victim) {
-    cost = 20;
-    if (hasEnoughMana(cost)) {
-      activ_power = "darkVision"
+    let cost = 20;
+    if (this.hasEnoughMana(cost)) {
       this.mana -= cost ;
       console.log("Utilisation du pouvoir Dark Vision");
       victim.takeDamage(5)
+      this.shield = 2
     }
-      //bouclier  =2 jusqu'au prochain tour
+    
   } 
 
 }
 
 class Paladin extends Character {
-  constructor(hp=16, dmg=3, mana=160,name="Ulder",status){
-    super(hp,dmg,mana,name,status)
+  constructor(hp=16, dmg=3, mana=160,name="Ulder",status,shield){
+    super(hp,dmg,mana,name,status,shield)
     this.class_name = "Paladin";
   }
 
   healingLighting(victim) {
-    victim.takeDamage(4)
-    this.hp += 5
+    let cost = 40;
+    if (this.hasEnoughMana(cost)) {
+      victim.takeDamage(4)
+      this.hp += 5
+      this.mana -= cost
+    }
+
   }
 }
 
 class Monk extends Character {
-  constructor(hp=8, dmg=4, mana=0,name="Moana",status){
-    super(hp,dmg,mana,name,status)
+  constructor(hp=8, dmg=4, mana=200,name="Moana",status,shield){
+    super(hp,dmg,mana,name,status,shield)
     this.class_name = "Monk";
+  }
+
+  heal(){
+    let cost = 25;
+    if (this.hasEnoughMana(cost)) {
+      this.hp += 8;
+      this.mana -= cost;
+    }
   }
 }
 
 class Berzerker extends Character {
-  constructor(hp=8, dmg=4, mana=0,name="Draven",status){
-    super(hp,dmg,mana,name,status)
+  constructor(hp=8, dmg=4, mana=0,name="Draven",status,shield){
+    super(hp,dmg,mana,name,status,shield);
     this.class_name = "Berzerker";
+  }
+
+  rage(){
+    this.dmg +=1;
+    this.hp -=1;
   }
 
 }
 class Assassin extends Character {
-  constructor(hp=6, dmg=6, mana=20,name="Carl",status){
-    super(hp,dmg,mana,name,status)
+  constructor(hp=6, dmg=6, mana=20,name="Carl",status,shield){
+    super(hp,dmg,mana,name,status,shield)
     this.class_name = "Assassin";
   }
+  shadowHit(victim){
+    let cost = 20
+    if (this.hasEnoughMana(cost)) {
+      victim.takeDamage(7);
+      this.mana -= cost;
+      this.shield=100
+    }
+  }
+
 }
